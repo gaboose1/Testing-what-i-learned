1ba1d4179c604eee2fe43ba8315eddd4379519af
gaboose1 <maankaasad98@gmail.com>
2026-07-19 23:08:26 +0300

    added abdi.py

---
+++ abdi.py
@@ -0,0 +1,16 @@
+# this one un meesha caadi u galin data 
+store_value = 100
+lacagta_maqan = 25.5
+yaa_qaatay = "abdulahi"
+active = True
+
+# after aad data galisid baa si fudud ku ogaan data typekeeda 
+
+print(type(store_value), store_value)
+print(type(lacagta_maqan), lacagta_maqan)
+print(type(yaa_qaatay), yaa_qaatay)
+print(type(active), active)
+
+
+# so i think know you got it
+
